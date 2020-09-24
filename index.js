08d731c912f77171a791406f97369b338172266d
latinrev <=>
2020-09-24 00:20:22 -0400

    :sparkles: Added support to download videos with emoji in title :sparkles:

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ytdl = require("ytdl-core");
 const SM = require("./ServerManager");
 const app = new SM(process.env.PORT || 5500, false, "/public").StartServer().GetApp();
-
+const emojiStrip = require("emoji-strip");
 app.get("/download", async (req, res) => {
 	const { link, type } = req.query;
 	if (ytdl.validateURL(link)) {
@@ -9,9 +9,10 @@ app.get("/download", async (req, res) => {
 			videoDetails: { title },
 		} = await ytdl.getBasicInfo(link);
 		const audio = type === "mp4" ? ytdl(link) : ytdl(link, { quality: "highestaudio" });
+		let cleanTitle = title !== "" ? emojiStrip(title.toString("utf8")).trim() : "No name video";
 		res.writeHead(200, {
 			"Content-Type": "application/octet-stream",
-			"Content-Disposition": `attachment; filename="${title.toString('utf8')}.${type}"`,
+			"Content-Disposition": `attachment; filename="${cleanTitle}.${type}"`,
 		});
 		audio.pipe(res);
 	} else {
